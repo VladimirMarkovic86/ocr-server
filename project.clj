2af4bda7c0fcb33f7fa02c43144669ea4f5854a2
Markovic Vladimir <markovic.vladimir86@gmail.com>
2018-08-21 19:28:36 +0200

    Reformatting project.clj

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
-(defproject ocr-server "0.1.0"
+(defproject org.vladimir/ocr-server "0.1.0"
   :description "Optical character recognition server"
   :url "http://gitlab:1610/VladimirMarkovic86/ocr-server"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure			   "1.9.0"]
-         									;	https://clojure.org/api/api
+  :dependencies [[org.clojure/clojure	"1.9.0"]
          								[org.vladimir/server-lib "0.1.0"]
          								[org.vladimir/websocket-server-lib "0.1.0"]
          								[org.vladimir/mongo-lib "0.1.0"]
@@ -18,4 +17,5 @@
   ; AOT - Compailation ahead of time
   :main ^:skip-aot ocr-server.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :repl-options {:port 8602})
