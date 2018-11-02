431511db1584e326ab140baca9d9b45fd87d5f35
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2018-11-02 17:20:33 +0100

    Changed version and implemented database update mechanism.

--- project.clj
+++ project.clj
@@ -1,18 +1,19 @@
-(defproject org.clojars.vladimirmarkovic86/ocr-server "0.2.0"
+(defproject org.clojars.vladimirmarkovic86/ocr-server "0.2.1"
   :description "Optical character recognition server"
   :url "http://github.com/VladimirMarkovic86/ocr-server"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojars.vladimirmarkovic86/server-lib "0.3.8"]
-                 [org.clojars.vladimirmarkovic86/mongo-lib "0.2.0"]
+                 [org.clojars.vladimirmarkovic86/mongo-lib "0.2.2"]
                  [org.clojars.vladimirmarkovic86/utils-lib "0.4.0"]
                  [org.clojars.vladimirmarkovic86/ajax-lib "0.1.0"]
-                 [org.clojars.vladimirmarkovic86/ocr-lib "0.2.0"]
-                 [org.clojars.vladimirmarkovic86/session-lib "0.2.1"]
-                 [org.clojars.vladimirmarkovic86/common-server "0.3.2"]
-                 [org.clojars.vladimirmarkovic86/ocr-middle "0.2.0"]
-                 [org.clojars.vladimirmarkovic86/audit-lib "0.1.2"]
+                 [org.clojars.vladimirmarkovic86/ocr-lib "0.2.1"]
+                 [org.clojars.vladimirmarkovic86/session-lib "0.2.2"]
+                 [org.clojars.vladimirmarkovic86/common-server "0.3.3"]
+                 [org.clojars.vladimirmarkovic86/ocr-middle "0.3.1"]
+                 [org.clojars.vladimirmarkovic86/common-middle "0.2.1"]
+                 [org.clojars.vladimirmarkovic86/audit-lib "0.1.3"]
                  ]
 
   :min-lein-version "2.0.0"
