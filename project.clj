d58de58b0f2a73581ca6ea03d0db95584c27850e
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-02-18 17:07:04 +0100

    Task 42: Adapted new design to ide project

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
                  [org.clojars.vladimirmarkovic86/ajax-lib "0.1.5"]
                  [org.clojars.vladimirmarkovic86/ocr-lib "0.2.4"]
                  [org.clojars.vladimirmarkovic86/session-lib "0.2.9"]
-                 [org.clojars.vladimirmarkovic86/common-server "0.3.15"]
+                 [org.clojars.vladimirmarkovic86/common-server "0.3.16"]
                  [org.clojars.vladimirmarkovic86/ocr-middle "0.3.5"]
                  [org.clojars.vladimirmarkovic86/common-middle "0.2.5"]
                  [org.clojars.vladimirmarkovic86/audit-lib "0.1.12"]
