6e55db76b31d090114f8ecdaf23915edc0845f5b
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-05-31 16:44:36 +0200

    Task 63: Implemented tests for language-lib.core.clj and language-lib.core.cljs namespaces.

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
                  [org.clojars.vladimirmarkovic86/ajax-lib "0.1.9"]
                  [org.clojars.vladimirmarkovic86/ocr-lib "0.2.13"]
                  [org.clojars.vladimirmarkovic86/session-lib "0.2.19"]
-                 [org.clojars.vladimirmarkovic86/common-server "0.3.34"]
+                 [org.clojars.vladimirmarkovic86/common-server "0.3.35"]
                  [org.clojars.vladimirmarkovic86/ocr-middle "0.3.7"]
                  [org.clojars.vladimirmarkovic86/common-middle "0.2.7"]
                  [org.clojars.vladimirmarkovic86/audit-lib "0.1.27"]
